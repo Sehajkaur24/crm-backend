e2b0cf3425ff9fc4d4807ea76ee81e9fbed7fcbf
Harmanpreet Singh <harmanpreet181001@gmail.com>
2025-08-16 11:57:21 +0530

    feat: get tasks by organisation

--- mount/app/api/v1/organisation_api.py
+++ mount/app/api/v1/organisation_api.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from app.common import responses
 from app.dependencies.db_dependency import AsyncSessionDep
+from app.schemas.task_schema import TaskRead
 from app.schemas.user_schema import EmployeeCreateRequest, UserRead
 from app.services import organisation_service
 
@@ -26,4 +27,13 @@ async def add_user_to_organisation(
 )
 async def get_organisation_users(db: AsyncSessionDep, org_id: int):
     res = await organisation_service.get_organisation_users(db=db, org_id=org_id)
+    return responses.success(data=res)
+
+
+@router.get(
+    "/organisations/{org_id}/tasks",
+    response_model=responses.ResponseModel[list[TaskRead]],
+)
+async def get_organisation_tasks(db: AsyncSessionDep, org_id: int):
+    res = await organisation_service.get_organisation_tasks(db=db, org_id=org_id)
     return responses.success(data=res)
--- mount/app/services/organisation_service.py
+++ mount/app/services/organisation_service.py
@@ -1,7 +1,9 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.database.models.task import Task
 from app.database.models.user import User
 from app.exceptions.user_exception import UserAlreadyExistException
+from app.repositories.task_repository import task_repo
 from app.repositories.user_repository import user_repo
 from app.schemas.user_schema import EmployeeCreateRequest, UserCreateWithHash, UserRead
 
@@ -35,3 +37,8 @@ async def get_organisation_users(db: AsyncSession, org_id: int) -> list[User]:
         filters={"organisation_id": org_id, "user_type": "employee"},
     )
     return users
+
+
+async def get_organisation_tasks(db: AsyncSession, org_id: int) -> list[Task]:
+    tasks = await task_repo.get_multi_by_attribute(db=db, attribute="organisation_id", value=org_id)
+    return tasks
